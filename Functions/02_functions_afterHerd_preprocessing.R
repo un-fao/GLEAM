e058fbbb95754a477b0755dea59d7b24331323b1
ahmedjoubest <ahmedjoubest@gmail.com>
2025-05-09 14:02:29 +0100

    Run styler on after herd scripts

--- Functions/02_functions_afterHerd_preprocessing.R
+++ Functions/02_functions_afterHerd_preprocessing.R
@@ -1,51 +1,41 @@
-
 # Function to calculate cohort-specific weights at different lifestage
-get.stepLW <- function (Animal_short, cohort, AFKG, AMKG, CKG, MFSKG, MMSKG, WKG, AFC, WA) {
+get.stepLW <- function(Animal_short, cohort, AFKG, AMKG, CKG, MFSKG, MMSKG, WKG, AFC, WA) {
   if (cohort %in% c("FJ")) {
-    initialLW= CKG
+    initialLW <- CKG
     if (Animal_short %in% c("PGS", "CML")) {
-      potfinalLW = slaughterLW = WKG
+      potfinalLW <- slaughterLW <- WKG
+    } else if (Animal_short %in% c("CTL", "BFL", "SHP", "GTS")) {
+      potfinalLW <- slaughterLW <- ((AFKG - CKG) / AFC) * WA + CKG
     }
-    else if (Animal_short %in% c("CTL", "BFL", "SHP", "GTS" )) {
-      potfinalLW = slaughterLW = ((AFKG-CKG)/AFC)*WA+CKG
-    }
-  }
-  else if (cohort %in% c("MJ")) {
-    initialLW= CKG
-    if (Animal_short %in% c("PGS","CML")) {
-      potfinalLW = slaughterLW = WKG
-    }
-    else if (Animal_short %in% c("CTL", "BFL", "SHP", "GTS")) {
-      potfinalLW = slaughterLW = ((AMKG-CKG)/AFC)*WA+CKG
-    }
-  }
-  else if (cohort == "FS") {
+  } else if (cohort %in% c("MJ")) {
+    initialLW <- CKG
     if (Animal_short %in% c("PGS", "CML")) {
-      initialLW = WKG
+      potfinalLW <- slaughterLW <- WKG
+    } else if (Animal_short %in% c("CTL", "BFL", "SHP", "GTS")) {
+      potfinalLW <- slaughterLW <- ((AMKG - CKG) / AFC) * WA + CKG
     }
-    else if (Animal_short %in% c("CTL", "BFL", "SHP", "GTS")) {
-      initialLW = ((AFKG-CKG)/AFC)*WA+CKG
-    }
-    potfinalLW = AFKG
-    slaughterLW = MFSKG
-  }
-  else if (cohort == "MS") {
+  } else if (cohort == "FS") {
     if (Animal_short %in% c("PGS", "CML")) {
-      initialLW = WKG
+      initialLW <- WKG
+    } else if (Animal_short %in% c("CTL", "BFL", "SHP", "GTS")) {
+      initialLW <- ((AFKG - CKG) / AFC) * WA + CKG
     }
-    else if (Animal_short %in% c("CTL", "BFL", "SHP", "GTS")) {
-      initialLW = ((AMKG-CKG)/AFC)*WA+CKG
+    potfinalLW <- AFKG
+    slaughterLW <- MFSKG
+  } else if (cohort == "MS") {
+    if (Animal_short %in% c("PGS", "CML")) {
+      initialLW <- WKG
+    } else if (Animal_short %in% c("CTL", "BFL", "SHP", "GTS")) {
+      initialLW <- ((AMKG - CKG) / AFC) * WA + CKG
     }
-    potfinalLW = AMKG
-    slaughterLW = MMSKG
+    potfinalLW <- AMKG
+    slaughterLW <- MMSKG
+  } else if (cohort == "FA") {
+    initialLW <- potfinalLW <- slaughterLW <- AFKG
+  } else if (cohort == "MA") {
+    initialLW <- potfinalLW <- slaughterLW <- AMKG
   }
-  else if (cohort == "FA") {
-    initialLW = potfinalLW = slaughterLW = AFKG
-  } 
-  else if (cohort == "MA") {
-    initialLW = potfinalLW = slaughterLW = AMKG
-  }
-  
+
   ret <- list(initialLW, potfinalLW, slaughterLW)
   names(ret) <- c("initialLW", "potfinalLW", "slaughterLW")
   return(ret)
@@ -53,14 +43,14 @@ get.stepLW <- function (Animal_short, cohort, AFKG, AMKG, CKG, MFSKG, MMSKG, WKG
 
 # Function to calculate cohort-specific average and final weights
 get.otherLW <- function(initialLW, potfinalLW, slaughterLW, offtake_rate) {
-  averageLW = (initialLW+(potfinalLW*(1-offtake_rate)+slaughterLW*(offtake_rate)))/2
-  finalLW = potfinalLW*(1-offtake_rate)+slaughterLW*(offtake_rate)
-  ret = list(averageLW, finalLW)
+  averageLW <- (initialLW + (potfinalLW * (1 - offtake_rate) + slaughterLW * (offtake_rate))) / 2
+  finalLW <- potfinalLW * (1 - offtake_rate) + slaughterLW * (offtake_rate)
+  ret <- list(averageLW, finalLW)
   names(ret) <- c("averageLW", "finalLW")
   return(ret)
 }
 
 get.dwg <- function(potfinalLW, initialLW, duration) {
-  dwg = (potfinalLW-initialLW)/duration
+  dwg <- (potfinalLW - initialLW) / duration
   return(dwg)
 }
--- Scripts/script4_AfterHerd_proc.R
+++ Scripts/script4_AfterHerd_proc.R
@@ -6,23 +6,22 @@ source("Functions/02_functions_afterHerd_preprocessing.R")
 
 # upload inputs
 out_herd.dt <- fread("Inputs//GLEAM_input_herdproc.csv")
-# afmilk <- fread("Inputs//FAOSTAT_data_milk_producing2015.csv")
 
-
-id.vars <- c("LPS", "HerdType", "Animal", "ADM0_CODE", "ISO3", "ISO3_num", "M49_code", "RegionClass", "COUNTRY") # columns as id
+id.vars <- c(
+  "LPS", "HerdType", "Animal", "ADM0_CODE", "ISO3", "ISO3_num", "M49_code", "RegionClass", "COUNTRY"
+) # columns as id
 cohort.vars <- c(
   "offtake_rate.FJ", "offtake_rate.FS", "offtake_rate.FA", "offtake_rate.MJ", "offtake_rate.MS", "offtake_rate.MA",
   "mort_rate.FJ", "mort_rate.FS", "mort_rate.FA", "mort_rate.MJ", "mort_rate.MS", "mort_rate.MA",
   "duration.FJ", "duration.FS", "duration.FA", "duration.MJ", "duration.MS", "duration.MA",
-  "share.FJ", "share.FS", "share.FA", "share.MJ", "share.MS", "share.MA", 
-  "size.FJ", "size.FS", "size.FA",  "size.MJ", "size.MS", "size.MA", 
-  "offtake_number.FJ", "offtake_number.FS", "offtake_number.FA",  "offtake_number.MJ",  "offtake_number.MS",  "offtake_number.MA"
+  "share.FJ", "share.FS", "share.FA", "share.MJ", "share.MS", "share.MA",
+  "size.FJ", "size.FS", "size.FA", "size.MJ", "size.MS", "size.MA",
+  "offtake_number.FJ", "offtake_number.FS", "offtake_number.FA", "offtake_number.MJ", "offtake_number.MS", "offtake_number.MA"
 )
 
 herd_pop <- out_herd.dt[, .SD, .SDcols = setdiff(names(out_herd.dt), cohort.vars)]
 
 herd_tolong <- out_herd.dt[, .SD, .SDcols = c(id.vars, cohort.vars)]
-
 
 # Reshape from wide to long format
 long_format <- melt(
@@ -33,17 +32,18 @@ long_format <- melt(
 )
 
 # Split the 'variable' into 'item' and 'cohort' using a custom approach
-long_format[, `:=` (
-  item = sub("\\..*$", "", variable),             # Remove everything after the period
-  cohort = ifelse(grepl("\\.", variable),          # Check if there is a period
-                  sub("^[^.]*\\.", "", variable),  # Remove everything before and including the period
-                  NA_character_)                   # Assign NA if there is no period
+long_format[, `:=`(
+  item = sub("\\..*$", "", variable), # Remove everything after the period
+  cohort = ifelse(grepl("\\.", variable), # Check if there is a period
+    sub("^[^.]*\\.", "", variable), # Remove everything before and including the period
+    NA_character_
+  ) # Assign NA if there is no period
 )]
 
 
 long_format <- dcast(
   long_format,
-  LPS + HerdType + Animal + ADM0_CODE + ISO3 + ISO3_num + RegionClass + COUNTRY + M49_code + cohort + RegionClass  ~ item,
+  LPS + HerdType + Animal + ADM0_CODE + ISO3 + ISO3_num + RegionClass + COUNTRY + M49_code + cohort + RegionClass ~ item,
   value.var = "value"
 )
 
@@ -58,26 +58,30 @@ setcolorder(GLEAM_input_feed_preproc, c(
 
 
 # add weights----
-GLEAM_input_feed_preproc[,c("initialLW", "potfinalLW", "slaughterLW"):=
-              get.stepLW(Animal_short, cohort, AFKG, AMKG, CKG=ckg, MFSKG, MMSKG, WKG=wkg, AFC=afc, WA), by=.I]
+GLEAM_input_feed_preproc[, c("initialLW", "potfinalLW", "slaughterLW") :=
+  get.stepLW(Animal_short, cohort, AFKG, AMKG, CKG = ckg, MFSKG, MMSKG, WKG = wkg, AFC = afc, WA), by = .I]
 
-GLEAM_input_feed_preproc[,c("averageLW", "finalLW"):= get.otherLW(initialLW, potfinalLW, slaughterLW, offtake_rate), by=.I]
+GLEAM_input_feed_preproc[, c("averageLW", "finalLW") := get.otherLW(initialLW, potfinalLW, slaughterLW, offtake_rate), by = .I]
 
 # add new daily weight gain----
-GLEAM_input_feed_preproc[,"dwg":= get.dwg(potfinalLW, initialLW, duration), by=.I]
+GLEAM_input_feed_preproc[, "dwg" := get.dwg(potfinalLW, initialLW, duration), by = .I]
 
 # new column need to be generated with WKG also for other species than PIGS (it is used in energy requirements)
 # FS and MS have the same weaning weight - filtering only for one
-weaning.dt<-GLEAM_input_feed_preproc[cohort %in% c("FS") & Animal_short!="PGS", .(COUNTRY, ADM0_CODE, Animal_short, LPS_short, HerdType_short, cohort,
-                                                                                  initialLW)]
+weaning.dt <- GLEAM_input_feed_preproc[cohort %in% c("FS") & Animal_short != "PGS", .(
+  COUNTRY, ADM0_CODE, Animal_short, LPS_short, HerdType_short, cohort,
+  initialLW
+)]
 GLEAM_input_feed_preproc[
   Animal_short != "PGS",
   wkg := weaning.dt[.SD, on = .(COUNTRY, ADM0_CODE, Animal_short, LPS_short, HerdType_short), initialLW]
 ]
 
 # select only needed columns
-drop <- c("AFCM", "AFKG", "AMKG", "BCR", "DR1M", "DR2", "DRF", "DRR2A", "DRR2B", 
-          "DWG2", "FRRF", "LW", "M2SKG", "MFSKG", "RRF", "RRM", "WA", "MMSKG")
+drop <- c(
+  "AFCM", "AFKG", "AMKG", "BCR", "DR1M", "DR2", "DRF", "DRR2A", "DRR2B",
+  "DWG2", "FRRF", "LW", "M2SKG", "MFSKG", "RRF", "RRM", "WA", "MMSKG"
+)
 
 GLEAM_input_feed_preproc <- GLEAM_input_feed_preproc[, !..drop]
 
