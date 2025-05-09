5ec44b7a413f4e06853888709c60fb29adea763d
ahmedjoubest <ahmedjoubest@gmail.com>
2025-05-09 13:40:38 +0100

    Remove unused milk code logic

--- Scripts/script4_AfterHerd_proc.R
+++ Scripts/script4_AfterHerd_proc.R
@@ -75,29 +75,10 @@ GLEAM_input_feed_preproc[
   wkg := weaning.dt[.SD, on = .(COUNTRY, ADM0_CODE, Animal_short, LPS_short, HerdType_short), initialLW]
 ]
 
-
-
-# add milking_fraction----
-
-# /!\ C A U T I O N /!\
-# milking fraction is FAOSTAT milk producing animals/FA size
-# at this stage several entries have milking_fraction >1 - this occures when FAOSTAT milk animals are > that FA size (calculated by herd module)
-# this can be caused by two things: 1. we are still keeping HerdType (beef and dairy); 2. we need to check offtake rate and WA
-# the solution for now to avoid having this proportion > 1 is to manually adjust it, but this will need to be fixed!!!! 
-# 
-# GLEAM_input_feed_preproc <- function_milking_fraction(GLEAM_input_feed_preproc, afmilk) #returning the following new columns:
-# GLEAM_input_feed_preproc[milking_fraction > 1, milking_fraction := 1] # /!\ C A U T I O N /!\ - THIS WILL NEED TO BE REMOVED / FORCING VALUES TO BE FROM 0 TO 1
-
-
-
 # select only needed columns
 drop <- c("AFCM", "AFKG", "AMKG", "BCR", "DR1M", "DR2", "DRF", "DRR2A", "DRR2B", 
           "DWG2", "FRRF", "LW", "M2SKG", "MFSKG", "RRF", "RRM", "WA", "MMSKG")
 
 GLEAM_input_feed_preproc <- GLEAM_input_feed_preproc[, !..drop]
 
-
-
 fwrite(GLEAM_input_feed_preproc, "Inputs/GLEAM_input_feed.csv")
-
-
