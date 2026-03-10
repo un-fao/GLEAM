bfa32602f055cbd3fad34608207847c47030d473
lydialanzoni <lydia.lanzoni@fao.org>
2026-03-10 16:05:31 +0100

    Restructuring result tables and variable lists by variable_type

--- R/run_aggregation.R
+++ R/run_aggregation.R
@@ -162,24 +162,124 @@ run_aggregation <- function(
   }
 
   # --- Step 1: Define variable groups -----------------------------------------
-  feed_vars <- c("dry_matter_intake")
-  nitrogen_balance_vars <- c("nitrogen_intake", "nitrogen_retention", "nitrogen_excretion")
-  production_vars <- c(
-    "milk_production_mass_cohort", "milk_production_protein_cohort",
-    "milk_production_fpcm_cohort",
-    "meat_production_live_weight_cohort", "meat_production_carcass_weight_cohort",
-    "meat_production_bone_free_meat_cohort", "meat_production_protein_cohort",
-    "fibre_production_cohort"
+  feed_list <- list(
+    list(
+      feed_source = "dry_matter_intake",
+      label = "DryMatterIntake",
+      unit = "kg dry matter"
+    )
   )
-  emissions_vars <- c(
-    "ch4_enteric", "ch4_manure_pasture", "ch4_manure_burned", "ch4_manure_other",
-    "n2o_manure_pasture_direct", "n2o_manure_burned_direct", "n2o_manure_other_direct",
-    "n2o_manure_burned_indirect", "n2o_manure_pasture_indirect", "n2o_manure_other_indirect",
-    "diet_co2_feed_fertilizer", "diet_co2_feed_pesticides", "diet_co2_feed_crop_operations",
-    "diet_co2_feed_luc_nopeat", "diet_co2_feed_luc_peat", "diet_n2o_feed_fertilizer",
-    "diet_n2o_feed_manure_applied", "diet_n2o_feed_crop_residues", "diet_ch4_feed_rice"
+  feed_vars <- sapply(feed_list, `[[`, "feed_source")
+  
+  
+  nitrogen_balance_list <- list(
+    list(
+      nitrogen_balance_source = "nitrogen_intake",
+      label = "NitrogenIntake",
+      unit = "kg N"
+    ),
+    list(
+      nitrogen_balance_source = "nitrogen_retention",
+      label = "NitrogenRetention",
+      unit = "kg N"
+    ),
+    list(
+      nitrogen_balance_source = "nitrogen_excretion",
+      label = "NitrogenExcretion",
+      unit = "kg N"
+    )
   )
-
+  nitrogen_balance_vars <- sapply(nitrogen_balance_list, `[[`, "nitrogen_balance_source")
+  
+  production_list <- list(
+    list(
+      production_source = "milk_production_mass_cohort",
+      label = "MilkRaw",
+      unit = "kg",
+      commodity_name = "Milk",
+      commodity_type = "Edible"
+    ),
+    list(
+      production_source = "milk_production_protein_cohort",
+      label = "MilkProtein",
+      unit = "kg protein",
+      commodity_name = "Milk",
+      commodity_type = "Edible"
+    ),
+    list(
+      production_source = "milk_production_fpcm_cohort",
+      label = "MilkFatProteinCorrected",
+      unit = "kg fat-protein corrected",
+      commodity_name = "Milk",
+      commodity_type = "Edible"
+    ),
+    list(
+      production_source = "meat_production_live_weight_cohort",
+      label = "MeatLiveWeight",
+      unit = "kg live weight",
+      commodity_name = "Meat",
+      commodity_type = "Edible"
+    ),
+    list(
+      production_source = "meat_production_carcass_weight_cohort",
+      label = "MeatCarcassWeight",
+      unit = "kg carcass weight",
+      commodity_name = "Meat",
+      commodity_type = "Edible"
+    ),
+    list(
+      production_source = "meat_production_bone_free_meat_cohort",
+      label = "MeatBoneFree",
+      unit = "kg bone-free meat",
+      commodity_name = "Meat",
+      commodity_type = "Edible"
+    ),
+    list(
+      production_source = "meat_production_protein_cohort",
+      label = "MeatProtein",
+      unit = "kg protein",
+      commodity_name = "Meat",
+      commodity_type = "Edible"
+    ),
+    list(
+      production_source = "fibre_production_cohort",
+      label = "Fibre",
+      unit = "kg",
+      commodity_name = "Fibre",
+      commodity_type = "Edible"
+    )
+  )
+  production_vars <- sapply(production_list, `[[`, "production_source")
+  
+  emissions_list <- list(
+    list(emissions_source = "ch4_enteric", label = "Enteric_CH4"),
+    list(emissions_source = "ch4_manure_pasture", label = "Manure-Pasture_CH4"),
+    list(emissions_source = "ch4_manure_burned", label = "Manure-Burned_CH4"),
+    list(emissions_source = "ch4_manure_other", label = "Manure-Other_CH4"),
+    
+    list(emissions_source = "n2o_manure_pasture_direct", label = "ManureDirect-Pasture_N2O"),
+    list(emissions_source = "n2o_manure_burned_direct", label = "ManureDirect-Burned_N2O"),
+    list(emissions_source = "n2o_manure_other_direct", label = "ManureDirect-Other_N2O"),
+    
+    list(emissions_source = "n2o_manure_burned_indirect", label = "ManureIndirect-Burned_N2O"),
+    list(emissions_source = "n2o_manure_pasture_indirect", label = "ManureIndirect-Pasture_N2O"),
+    list(emissions_source = "n2o_manure_other_indirect", label = "ManureIndirect-Other_N2O"),
+    
+    list(emissions_source = "diet_co2_feed_fertilizer", label = "Feed-Fertilizer_CO2"),
+    list(emissions_source = "diet_co2_feed_pesticides", label = "Feed-Pesticides_CO2"),
+    list(emissions_source = "diet_co2_feed_crop_operations", label = "Feed-CropOperations_CO2"),
+    list(emissions_source = "diet_co2_feed_luc_nopeat", label = "Feed-LandUseChange_CO2"),
+    list(emissions_source = "diet_co2_feed_luc_peat", label = "Feed-PeatDrainage_CO2"),
+    
+    list(emissions_source = "diet_n2o_feed_fertilizer", label = "Feed-Fertilizer_N2O"),
+    list(emissions_source = "diet_n2o_feed_manure_applied", label = "Feed-ManureApplication_N2O"),
+    list(emissions_source = "diet_n2o_feed_crop_residues", label = "Feed-CropResidues_N2O"),
+    
+    list(emissions_source = "diet_ch4_feed_rice", label = "Feed-Rice_CH4")
+  )
+  emissions_vars <- sapply(emissions_list, `[[`, "emissions_source")
+  
+  
   # Check that required variables exist in cohort_level_data
   all_vars <- unique(
     c(feed_vars, nitrogen_balance_vars, production_vars, emissions_vars)
@@ -206,6 +306,8 @@ run_aggregation <- function(
     value.name = "value"
   )
 
+  
+  
   # --- Step 3: Classify variables by type -------------------------------------
   data_cohort_long[
     , variable_type := data.table::fcase(
@@ -243,25 +345,30 @@ run_aggregation <- function(
     cohort_short = "cohort_short"
   )
 
-  # --- Step 6: Merge emissions with allocation data ---------------------------
+  # --- Step 6: Subsetting datframes by variable_type ----------------------------
+  data_herd_long_production <- data_herd_long[variable_type=="Production"]
+  data_herd_long_nitrogen <- data_herd_long[variable_type=="NitrogenBalance"]
+  data_herd_long_feed<- data_herd_long[variable_type=="Feed"]
+  
+  # --- Step 7: Subsetting emissions dataframe and merge emissions with allocation data ---------------------------
   # Only emissions need allocation; other variables are assigned to "ALL"
-  data_herd_long_allocation <- merge(
-    data_herd_long[variable_type == "Emissions", ],
+  data_herd_long_emissions<- merge(
+    data_herd_long[variable_type == "Emissions", .(herd_id, species_short, variable_type, variable_name, value_total_kgGas=value_total)],
     allocation_herd_long,
     by = c("herd_id", "species_short", "variable_name")
   )
 
-  # --- Step 7: Allocate emissions to commodities ------------------------------
-  data_herd_long_allocation[
-    , value_allocated := calc_allocated_emissions(
-      value = value_total,
+  # --- Step 8: Allocate emissions to commodities ------------------------------
+  data_herd_long_emissions[
+    , value_total_allocated_kgGas := calc_allocated_emissions(
+      value = value_total_kgGas,
       allocation_share = allocation_share
     ),
     by = .I
   ]
 
-  # --- Step 8: Identify gas type for GWP conversion ---------------------------
-  data_herd_long_allocation[
+  # --- Step 9: Identify gas type for GWP conversion ---------------------------
+  data_herd_long_emissions[
     , gas := data.table::fcase(
       grepl("^ch4", variable_name, ignore.case = TRUE), "CH4",
       grepl("n2o", variable_name, ignore.case = TRUE), "N2O",
@@ -269,160 +376,61 @@ run_aggregation <- function(
     )
   ]
 
-  # --- Step 9: Convert to CO2-equivalents -------------------------------------
-  data_herd_long_allocation[
-    , c("value_allocated_co2e", "gwp") := calc_co2eq(
+  # --- Step 10: Convert to CO2-equivalents -------------------------------------
+  data_herd_long_emissions[
+    , c("value_total_allocated_co2eq", "gwp") := calc_co2eq(
       gas = gas,
-      value_allocated = value_allocated,
+      value_allocated = value_total_allocated_kgGas,
       gwp = gwp
     ),
     by = .I
   ]
 
-  # --- Step 10: Cleaning-up emissions variables -------------------------------
-  subset_allocatedco2e <- data_herd_long_allocation[
-    ,
-    .(
-      herd_id, species_short,
-      variable_name, gas, variable_type, commodity_name,
-      allocation_share, commodity_type, value_total = value_allocated_co2e,
-      gwp
-    )
-  ]
-  subset_allocatedco2e[, unit := "kg co2eq"]
+  # --- Step 11: Cleaning-up output tables -----------------------------------------
+  
+  # 11.1 Emissions
+  emissions_dt <- data.table::rbindlist(emissions_list)
+  data.table::setnames(emissions_dt, "emissions_source", "variable_name")
+  
+  data_herd_long_emissions <- merge(data_herd_long_emissions,
+                                    emissions_dt,
+                                     by = "variable_name")
+  
 
-  # --- Step 11: Combine the Emissions results allocated with other variables --
-  results_herd <- data.table::rbindlist(
-    list(
-      subset_allocatedco2e,
-      data_herd_long[variable_type != "Emissions"]
-    ),
-    use.names = TRUE,
-    fill = TRUE
-  )
-
-  # --- Step 12: Cleaning-up the table -----------------------------------------
-
-  # 12.1 Production
-  results_herd[
-    , unit := data.table::fcase(
-      variable_name %in% c("milk_production_mass_cohort", "fibre_production_cohort"), "kg",
-      variable_name %in% c("milk_production_protein_cohort", "meat_production_protein_cohort"), "kg protein",
-      variable_name %in% c("milk_production_fpcm_cohort"), "kg fat-protein corrected",
-      variable_name %in% c("meat_production_live_weight_cohort"), "kg live weight",
-      variable_name %in% c("meat_production_carcass_weight_cohort"), "kg carcass weight",
-      variable_name %in% c("meat_production_bone_free_meat_cohort"), "kg bone-free meat",
-      default = unit
-    )
-  ]
-
-  results_herd[
-    , commodity_name := data.table::fcase(
-      variable_name %in% c("milk_production_mass_cohort", "milk_production_protein_cohort", "milk_production_fpcm_cohort"), "Milk",
-      variable_name %in% c("meat_production_live_weight_cohort", "meat_production_carcass_weight_cohort", "meat_production_bone_free_meat_cohort", "meat_production_protein_cohort"), "Meat",
-      variable_name == "fibre_production_cohort", "Fibre",
-      default = commodity_name
-    )
-  ][
-    , commodity_type := data.table::fcase(
-      variable_type %in% c("Production"), "Edible",
-      default = commodity_type
-    )
-  ]
-
-  # 12.2 Feed & N balance
-  results_herd[
-    , unit := data.table::fcase(
-      variable_name %in% c("dry_matter_intake"), "kg dry matter",
-      variable_name %in% c("nitrogen_intake", "nitrogen_retention", "nitrogen_excretion"), "kg N",
-      default = unit
-    )
-  ]
-
-  results_herd[
-    !variable_type %in% c("Emissions", "Production"),
-    commodity_name := "ALL"
-  ]
-
-  results_herd[
-    !variable_type %in% c("Emissions"),
-    allocation_share := 1
-  ]
-
-  results_herd[
-    !variable_type %in% c("Emissions"),
-    gwp := 1
-  ]
-
-  results_herd[, cohort_short := "ALL"]
-
-  # --- Step 13: Renaming variables --------------------------------------------
-  # Ensure variable_name is a factor for levels() assignment
-  if (!is.factor(results_herd$variable_name)) {
-    results_herd[, variable_name := as.factor(variable_name)]
-  }
-
-  levels(results_herd$variable_name) <- c(
-    size = "LivestockNumbers",
-    ch4_enteric = "Enteric_CH4",
-    ch4_manure_pasture = "Manure-Pasture_CH4",
-    ch4_manure_burned = "Manure-Burned_CH4",
-    ch4_manure_other = "Manure-Other_CH4",
-    n2o_manure_pasture_direct = "ManureDirect-Pasture_N2O",
-    n2o_manure_burned_direct = "ManureDirect-Burned_N2O",
-    n2o_manure_other_direct = "ManureDirect-Other_N2O",
-    n2o_manure_burned_indirect = "ManureIndirect-Burned_N2O",
-    n2o_manure_pasture_indirect = "ManureIndirect-Pasture_N2O",
-    n2o_manure_other_indirect = "ManureIndirect-Other_N2O",
-    diet_co2_feed_fertilizer = "Feed-Fertilizer_CO2",
-    diet_co2_feed_pesticides = "Feed-Pesticides_CO2",
-    diet_co2_feed_crop_operations = "Feed-CropOperations_CO2",
-    diet_co2_feed_luc_nopeat = "Feed-LandUseChange_CO2",
-    diet_co2_feed_luc_peat = "Feed-PeatDrainage_CO2",
-    diet_n2o_feed_fertilizer = "Feed-Fertilizer_N2O",
-    diet_n2o_feed_manure_applied = "Feed-ManureApplication_N2O",
-    diet_n2o_feed_crop_residues = "Feed-CropResidues_N2O",
-    diet_ch4_feed_rice = "Feed-Rice_CH4",
-    dry_matter_intake = "DryMatterIntake",
-    nitrogen_intake = "NitrogenIntake",
-    nitrogen_retention = "NitrogenRetention",
-    nitrogen_excretion = "NitrogenExcretion",
-    milk_production_mass_cohort = "MilkRaw",
-    milk_production_protein_cohort = "MilkProtein",
-    milk_production_fpcm_cohort = "MilkFatProteinCorrected",
-    meat_production_live_weight_cohort = "MeatLiveWeight",
-    meat_production_carcass_weight_cohort = "MeatCarcassWeight",
-    meat_production_bone_free_meat_cohort = "MeatBoneFree",
-    meat_production_protein_cohort = "MeatProtein",
-    fibre_production_cohort = "Fibre"
-  )[levels(results_herd$variable_name)]
-
-  # --- Step 14: Variables order -----------------------------------------------
-  variable_order <- c(
-    "herd_id",
-    "species_short",
-    "cohort_short",
-    "variable_type",
-    "variable_name",
-    "unit",
-    "gas",
-    "gwp",
-    "allocation_share",
-    "commodity_type",
-    "commodity_name",
-    "value_total"
-  )
-
-  data.table::setcolorder(
-    results_herd,
-    intersect(variable_order, names(results_herd))
-  )
+  # 12.2 Production
+  production_dt <- data.table::rbindlist(production_list)
+  data.table::setnames(production_dt, "production_source", "variable_name")
+  
+  data_herd_long_production <- merge(data_herd_long_production,
+                                    production_dt,
+                                    by = "variable_name")
+  
+  
+  # 12.3 Feed
+  feed_dt <- data.table::rbindlist(feed_list)
+  data.table::setnames(feed_dt, "feed_source", "variable_name")
+  
+  data_herd_long_feed <- merge(data_herd_long_feed,
+                                     feed_dt,
+                                     by = "variable_name")
+  
+  
+  # 12.4 Nitrogen balance
+  nitrogen_balance_dt <- data.table::rbindlist(nitrogen_balance_list)
+  data.table::setnames(nitrogen_balance_dt, "nitrogen_balance_source", "variable_name")
+  
+  data_herd_long_nitrogen <- merge(data_herd_long_nitrogen,
+                                   nitrogen_balance_dt,
+                               by = "variable_name")
 
   # --- Return results ---------------------------------------------------------
   return(
     list(
       cohort_level_results = cohort_level_data,
-      results_herd = results_herd
+      results_emissions = data_herd_long_emissions,
+      results_feed = data_herd_long_feed,
+      results_production = data_herd_long_production,
+      results_nitrogen = data_herd_long_nitrogen
     )
   )
 }
